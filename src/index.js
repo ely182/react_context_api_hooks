d11537c057924fcf22b13ed10a4ad3bb2b677d78
ely182 <ely.18gabriel@gmail.com>
2022-12-01 10:11:01 -0300

    aula

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { CounterContextProvider } from './contexts/CounterContext';
 import './styles/global-styles.css';
 
 import { Home } from './templates/Home';
@@ -7,6 +8,9 @@ import { Home } from './templates/Home';
 ReactDOM.render(
   <React.StrictMode>
     <Home />
+    <CounterContextProvider>
+      <Home />
+    </CounterContextProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,81 +1,11 @@
-import React, { Suspense, useState } from 'react';
-// import LazyComponent from './lazy-component';
-
-
-const loadComponent = () => {
-  console.log('carregando componente');
-  return import('./lazy-component');
-}
-const LazyComponent = React.lazy(() => import('./lazy-component'));
+import { useCounterContext } from '../../contexts/CounterContext';
 
 export const Home = () => {
-const [show, setShow] = useState(false);
+  const [state, dispatch] = useCounterContext();
 
   return (
-      <div>
-          <p>
-            <button onMouseOver={loadComponent} onClick={() => setShow((s)=> !s)}>
-              mostrar {show ? 'LC on screen' : 'LC is off'}
-            </button>
-          </p>
-          <Suspense fallback={<p>carregando...</p>}>{show && <LazyComponent/>}</Suspense>
-      </div>
-  )
-}
-
-
-
-
-//  * Compound Components
-// import { Children, cloneElement, createContext, useContext, useState } from 'react';
-
-// const s = {
-//   style: {
-//     fontSize: '60px',
-//   },
-// };
-
-// const TurnOnOffContext = createContext();
-
-
-// const TurnOnOff = ({ children }) => {
-//   const [isOn, setIsOn] = useState(false);
-//   const onTurn = () => setIsOn((s) => !s);
-
-//  return <TurnOnOffContext.Provider value={{isOn,onTurn}} >{children}</TurnOnOffContext.Provider>
-// };
-// const TurnedOn = ({ children }) => {
-//   const{isOn} = useContext(TurnOnOffContext)
-//   return isOn ? children : null
-// };
-// const TurnedOff = ({children }) => {
-//   const{isOn} = useContext(TurnOnOffContext)
-//   return isOn ? null : children
-// };
-// const TurnButton = ({ ...props }) => {
-//   const{isOn, onTurn} = useContext(TurnOnOffContext)
-//   return (
-//     <button onClick={onTurn} {...props}>
-//       Turn {isOn ? 'OFF' : 'ON'}
-//     </button>
-//   );
-// };
-// const P = ({ children }) => <p {...s}>{children}</p>;
-
-// export const Home = () => {
-//   return (
-//     <TurnOnOff>
-//       <div>
-//     <p>oi</p>
-//       <TurnedOn>
-//         <P>Aqui as coisas que vão acontecer quando estiver ON.</P>
-//       </TurnedOn>
-//       <TurnedOff>
-//         <P>Aqui vem as coisas do OFF.</P>
-//       </TurnedOff>
-//       <TurnButton {...s} />
-//       </div>
-  
-//     </TurnOnOff>
-//   );
-// };
+    <div>
+      <h1>Oi</h1>
+    </div>
+  );
+};
